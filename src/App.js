dc72216727e72180ee1684478937d7323b4d5f5b
Michał <bodysmic@gmail.com>
2022-12-14 13:38:23 +0100

    connect to server json and made routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Button } from 'react-bootstrap';
+
+import {  Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import Table from './components/pages/Table/Table';
+import Error from './components/pages/Error/Error';
+
 
 
 function App() {
   return (
-    <div className="App">
-       
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/table/:id" element={<Table />}/>
+        <Route path="*" element={<Error />}/>
+      </Routes>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+// eslint-disable-next-line
+import store from './redux/store'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
